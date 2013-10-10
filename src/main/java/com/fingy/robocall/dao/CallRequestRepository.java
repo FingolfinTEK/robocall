ef16392c296309a687334cde4f0580c9ca96581f
FingolfinTEK <fingolfintek@gmail.com>
2013-10-10 23:22:48 +0200

    Redial

--- src/main/java/com/fingy/robocall/dao/CallRequestRepository.java
+++ src/main/java/com/fingy/robocall/dao/CallRequestRepository.java
@@ -4,8 +4,6 @@ import com.fingy.robocall.model.CallRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CallRequestRepository extends JpaRepository<CallRequest, Long> {
-
     CallRequest findBySid(String sid);
 }
