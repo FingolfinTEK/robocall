dcce23bec752951a31068535dbc51275f656506a
FingolfinTEK <fingolfintek@gmail.com>
2013-10-13 13:32:24 +0200

    Redial fix

--- src/main/java/com/fingy/robocall/service/impl/TwilioServiceImpl.java
+++ src/main/java/com/fingy/robocall/service/impl/TwilioServiceImpl.java
@@ -16,7 +16,6 @@ import org.joda.time.DateTimeZone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Service;
 
